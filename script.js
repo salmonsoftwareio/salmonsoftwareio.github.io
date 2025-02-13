b93653bc55933f545db92e5f344f674871e0e945
h00p <mattsavidant2@gmail.com>
2025-02-13 13:55:54 -0400

    style: improved layout of styles

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Thank you for your message! We will get back to you soon.');
-    this.reset();
-}); 
